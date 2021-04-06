8b8365fa0911c9bf2e99ad188fb960c2c6c1b543
cmd08010 <colleendunion@gmail.com>
2021-04-06 13:06:11 -0400

    Create base app with empty components

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+
+function Contact() {
+  return (
+    <div class="col-md-auto">
+      Contact
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/RSVP.js
@@ -0,0 +1,12 @@
+import {
+  Redirect
+} from "react-router-dom";
+function RSVP() {
+  return (
+    <div>
+      <Redirect to="https://michaelandcolleen.planningpod.com"/>
+    </div>
+  );
+}
+
+export default RSVP;
